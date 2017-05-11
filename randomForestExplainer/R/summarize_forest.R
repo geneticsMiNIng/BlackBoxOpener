7b6a7c56cad901af30ac8dfd49a1097d1bd3b3c3
OlaPaluszynska <ola.paluszynska@gmail.com>
2017-05-11 12:08:20 +0200

    Update vignette, place summarize_forest template

--- randomForestExplainer/R/summarize_forest.R
+++ randomForestExplainer/R/summarize_forest.R
@@ -1,3 +1,4 @@
 summarize_forest <- function(){
-  rmarkdown::render("Summarize_forest_template.rmd", "html_document", envir = new.env(parent = globalenv()))
+  rmarkdown::render(paste0(path.package("randomForestExplainer"), "/templates/Summarize_forest_template.rmd"),
+                    "html_document", output_file = "Summary_of_your_forest.html", envir = new.env(parent = globalenv()))
 }
