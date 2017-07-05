8393640009230186463704d32c641d4754946068
OlaPaluszynska <ola.paluszynska@gmail.com>
2017-07-05 22:39:07 +0200

    Update vignette

--- randomForestExplainer/inst/doc/randomForestExplainer.R
+++ randomForestExplainer/inst/doc/randomForestExplainer.R
@@ -2,41 +2,39 @@
 knitr::opts_chunk$set(echo = TRUE, warning = FALSE, message = FALSE)
 
 ## ------------------------------------------------------------------------
-library(data.table)
-library(dplyr)
-library(ggplot2)
 library(randomForest)
 library(randomForestExplainer)
 
 ## ------------------------------------------------------------------------
 load("GlioblastomaWide.rda")
-
-## ------------------------------------------------------------------------
+GlioblastomaWide <- GlioblastomaWide[, -1]
 GlioblastomaWide$death1y <- as.factor(GlioblastomaWide$death1y)
-remove <- is.na(GlioblastomaWide) %>% colSums()
+remove <- colSums(is.na(GlioblastomaWide))
 GlioblastomaWide <- GlioblastomaWide[, remove == 0]
 rm(remove)
-GlioblastomaWide <- GlioblastomaWide[, -1]
 
 ## ------------------------------------------------------------------------
 # set.seed(2017)
-# forest <- randomForest(death1y ~ ., data = GlioblastomaWide, ntree = 10000, importance = TRUE)
+# forest <- randomForest(death1y ~ ., data = GlioblastomaWide, ntree = 10000, localImp = TRUE)
 # save(forest, file = "GlioblastomaWide_forest.rda")
 load("GlioblastomaWide_forest.rda")
 
 ## ------------------------------------------------------------------------
-min_depth_frame <- min_depth_distribution(forest)
-head(min_depth_frame, n = 10) # this is how the output looks like
+plot(forest, main = "Learning curve of the forest")
+legend("topright", c("error for 'dead'", "misclassification error", "error for 'alive'"), lty = c(1,1,1), col = c("green", "black", "red"))
 
 ## ------------------------------------------------------------------------
-# library(tidyr)
-# min_depth_frame_complete <- tidyr::complete(min_depth_frame, tree = 1:forest$ntree, variable = labels(forest$terms))
+forest
 
-## ---- fig.width = 7, fig.height = 5--------------------------------------
+## ------------------------------------------------------------------------
+min_depth_frame <- min_depth_distribution(forest)
+head(min_depth_frame, n = 10)
+
+## ------------------------------------------------------------------------
 plot_min_depth_distribution(min_depth_frame)
 
-## ---- fig.width = 7, fig.height = 7--------------------------------------
-plot_min_depth_distribution(min_depth_frame, k = 15, min_no_of_trees = 50)
+## ------------------------------------------------------------------------
+plot_min_depth_distribution(min_depth_frame, min_no_of_trees = 60, mean_sample = "relevant_trees")
 
 ## ------------------------------------------------------------------------
 # importance_frame <- measure_importance(forest)
@@ -44,71 +42,54 @@ plot_min_depth_distribution(min_depth_frame, k = 15, min_no_of_trees = 50)
 load("GlioblastomaWide_importance_frame.rda")
 head(importance_frame, n = 10)
 
-## ---- fig.width = 7, fig.height = 7--------------------------------------
-# Possible measures for classification: "mean_min_depth", "no_of_nodes", "accuracy_decrease", "gini_decrease", "no_of_trees", "times_a_root", "p_value"
-plot_multi_way_importance(importance_frame, x_measure = "mean_min_depth", 
-                          y_measure = "no_of_trees", size_measure = "gini_decrease", 
-                          min_no_of_trees = 0.2*max(importance_frame$no_of_trees),
-                          no_of_labels = 10, 
-                          main = "Multi-way importance plot")
-plot_multi_way_importance(importance_frame, x_measure = "mean_min_depth", 
-                          y_measure = "no_of_nodes", size_measure = "accuracy_decrease", 
-                          min_no_of_trees = 0.2*max(importance_frame$no_of_trees),
-                          no_of_labels = 10, 
-                          main = "Multi-way importance plot")
+## ------------------------------------------------------------------------
+plot_multi_way_importance(importance_frame, size_measure = "no_of_nodes", min_no_of_trees = 30)
 
-## ---- fig.width = 7, fig.height = 7--------------------------------------
-plot_multi_way_importance(importance_frame, x_measure = "mean_min_depth", 
-                          y_measure = "times_a_root", size_measure = "p_value", 
-                          min_no_of_trees = 0.2*max(importance_frame$no_of_trees),
-                          no_of_labels = 10, 
-                          main = "Multi-way importance plot")
-plot_multi_way_importance(importance_frame, x_measure = "mean_min_depth", 
-                          y_measure = "gini_decrease", size_measure = "times_a_root", 
-                          min_no_of_trees = 0.4*max(importance_frame$no_of_trees),
-                          no_of_labels = 15, 
-                          main = "Multi-way importance plot")
+## ------------------------------------------------------------------------
+plot_multi_way_importance(importance_frame, x_measure = "accuracy_decrease", y_measure = "gini_decrease", size_measure = "p_value")
 
-## ---- fig.width = 7, fig.height = 6--------------------------------------
+## ------------------------------------------------------------------------
 plot_importance_ggpairs(importance_frame)
 
-## ---- fig.width = 7, fig.height = 6--------------------------------------
+## ------------------------------------------------------------------------
 plot_importance_rankings(importance_frame)
 
 ## ------------------------------------------------------------------------
-vars <- important_variables(importance_frame, k = 20, measures = c("mean_min_depth", "no_of_trees"))
+(vars <- important_variables(importance_frame, k = 20, measures = c("mean_min_depth", "no_of_trees")))
 
 ## ------------------------------------------------------------------------
 # interactions_frame <- min_depth_interactions(forest, vars)
 # save(interactions_frame, file = "GlioblastomaWide_interactions_frame.rda")
 load("GlioblastomaWide_interactions_frame.rda")
-
-## ------------------------------------------------------------------------
-head(interactions_frame)
-
-## ------------------------------------------------------------------------
-head(interactions_frame[order(interactions_frame$occurances, decreasing = TRUE), ])
+head(interactions_frame[order(interactions_frame$occurrences, decreasing = TRUE), ])
 
 ## ------------------------------------------------------------------------
 # set.seed(2017)
 # forest_v2 <- randomForest(death1y ~ ., data = GlioblastomaWide, ntree = 10000, mtry = floor(ncol(GlioblastomaWide)/3), importance = TRUE, localImp = TRUE)
 # save(forest_v2, file = "GlioblastomaWide_forest_v2.rda")
-load("GlioblastomaWide_forest_v2.rda")
+load("GlioblastomaWide_forest_v2.rda"); rm(forest)
 importance_frame <- measure_importance(forest_v2)
 vars <- important_variables(importance_frame, k = 20, measures = c("mean_min_depth", "no_of_trees"))
 # interactions_frame <- min_depth_interactions(forest_v2, vars)
 # save(interactions_frame, file = "GlioblastomaWide_interactions_frame_v2.rda")
 load("GlioblastomaWide_interactions_frame_v2.rda")
-head(interactions_frame[order(interactions_frame$occurances, decreasing = TRUE), ])
+head(interactions_frame[order(interactions_frame$occurrences, decreasing = TRUE), ])
 
-## ---- fig.width = 7, fig.height = 5--------------------------------------
+## ------------------------------------------------------------------------
 plot_min_depth_interactions(interactions_frame)
 
-## ---- eval = FALSE-------------------------------------------------------
-#  summarize_forest(forest, interactions = TRUE)
+## ------------------------------------------------------------------------
+interactions_frame <- min_depth_interactions(forest_v2, vars, mean_sample = "relevant_trees", uncond_mean_sample = "relevant_trees")
+plot_min_depth_interactions(interactions_frame)
+
+## ------------------------------------------------------------------------
+plot_predict_interaction(forest_v2, GlioblastomaWide, "SLC17A9", "IFIT2", grid = 80)
 
 ## ---- eval = FALSE-------------------------------------------------------
-#  summarize_forest(forest_v2, interactions = TRUE)
+#  explain_forest(forest, interactions = TRUE, GlioblastomaWide, pred_grid = 70)
+
+## ---- eval = FALSE-------------------------------------------------------
+#  explain_forest(forest_v2, interactions = TRUE, GlioblastomaWide, pred_grid = 80)
 
 ## ---- eval = FALSE-------------------------------------------------------
 #  # source("https://bioconductor.org/biocLite.R")
@@ -132,7 +113,7 @@ plot_min_depth_interactions(interactions_frame)
 #  # forest_brca <- randomForest(SUBTYPE ~ ., data = brca, ntree = 10000, localImp = TRUE)
 #  # save(forest_brca, file = "BreastCancer_forest.rda")
 #  load("BreastCancer_forest.rda")
-#  summarize_forest(forest_brca, interactions = TRUE)
+#  explain_forest(forest_brca, interactions = TRUE, brca)
 
 ## ---- eval = FALSE-------------------------------------------------------
 #  # devtools::install_github("pbiecek/PISA2012lite")
@@ -140,25 +121,26 @@ plot_min_depth_interactions(interactions_frame)
 #  
 #  # pisa <- na.omit(student2012[,c(1, 4, 12, 13, 18:20, 39, 61:62, 114, 488, 457, 501)])
 #  # pisa <- pisa[pisa$CNT %in% c("Austria", "Belgium", "Czech Republic", "Germany", "Denmark", "Spain", "Estonia", "Finland", "France", "United Kingdom", "Greece", "Hungary", "Ireland", "Italy", "Lithuania", "Luxembourg", "Latvia", "Netherlands", "Poland", "Portugal", "Slovak Republic", "Slovenia", "Sweden", "Romania", "Croatia", "Bulgaria"),] # consider only EU countries to reduce the size
-#  # pisa <- pisa[pisa$CNT %in% c(), ] # only the Visegrad group to begin with
-#  # pisa$CNT <- factor(pisa$CNT)
-#  
 #  # save(pisa, file = "PISA.rda")
 #  load("PISA.rda")
+#  
+#  # Further reduce the size
+#  pisa <- pisa[pisa$CNT %in% c("Czech Republic", "Hungary", "Poland", "Slovak Republic"), ] # only the Visegrad group
+#  pisa$CNT <- factor(pisa$CNT)
 
 ## ---- eval = FALSE-------------------------------------------------------
 #  # set.seed(2017)
 #  # forest_pisa <- randomForest(PV1MATH ~ ., data = pisa, localImp = TRUE)
 #  # save(forest_pisa, file = "PISA_forest.rda")
 #  load("PISA_forest.rda")
-#  summarize_forest(forest_pisa, interactions = TRUE)
+#  explain_forest(forest_pisa, interactions = TRUE, pisa)
 
 ## ---- eval = FALSE-------------------------------------------------------
-#  # data(Boston, package = "MASS")
-#  # Boston$chas <- as.logical(Boston$chas)
+#  data(Boston, package = "MASS")
+#  Boston$chas <- as.logical(Boston$chas)
 #  # set.seed(2017)
 #  # forest_Boston <- randomForest(medv ~ ., data = Boston, ntree = 1000, localImp = TRUE)
 #  # save(forest_Boston, file = "Boston_forest.rda")
 #  load("Boston_forest.rda")
-#  summarize_forest(forest_Boston, interactions = TRUE)
+#  explain_forest(forest_Boston, interactions = TRUE, Boston)
 
